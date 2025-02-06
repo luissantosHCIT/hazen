d427213f79d3e0cbc17d7dd1686e995fe9b02e57
Luis M. Santos <luis.santos2@nih.gov>
2025-02-06 09:35:27 -0500

    Added ACR Object Detectability Task Unit tests

---
+++ tests/test_acr_object_detectability.py
@@ -0,0 +1,49 @@
+import os
+import unittest
+import pathlib
+import pydicom
+
+from hazenlib.utils import get_dicom_files
+from hazenlib.tasks.acr_object_detectability import ACRObjectDetectability
+from hazenlib.ACRObject import ACRObject
+from tests import TEST_DATA_DIR, TEST_REPORT_DIR
+
+
+class TestACRObjectDetectability(unittest.TestCase):
+    ACR_DATA = pathlib.Path(TEST_DATA_DIR / "acr" / "Siemens")
+    SCORE_8 = 0
+    TOTAL_SCORE = 30
+
+    def setUp(self):
+        input_files = get_dicom_files(self.ACR_DATA)
+        self.acr_object_detectability = ACRObjectDetectability(
+            input_data=input_files,
+            report_dir=pathlib.PurePath.joinpath(TEST_REPORT_DIR),
+        )
+        self.results = self.acr_object_detectability.run()
+
+    def test_slice_8_score(self):
+        slice_8 = self.results['slice_scores'][0]
+        assert slice_8 == self.SCORE_8
+
+    def test_total_score(self):
+        total_score = self.results['score']
+        assert total_score == self.TOTAL_SCORE
+
+
+class TestACRObjectDetectabilitySiemensSolaFit(TestACRObjectDetectability):
+    ACR_DATA = pathlib.Path(TEST_DATA_DIR / "acr" / "SiemensSolaFit")
+    SCORE_8 = 9
+    TOTAL_SCORE = 38
+
+
+class TestACRObjectDetectabilityGE(TestACRObjectDetectability):
+    ACR_DATA = pathlib.Path(TEST_DATA_DIR / "acr" / "GE")
+    SCORE_8 = 0
+    TOTAL_SCORE = 7
+
+
+class TestACRObjectDetectabilityPhilipsAchieva(TestACRObjectDetectability):
+    ACR_DATA = pathlib.Path(TEST_DATA_DIR / "acr" / "PhilipsAchieva")
+    SCORE_8 = 10
+    TOTAL_SCORE = 40
