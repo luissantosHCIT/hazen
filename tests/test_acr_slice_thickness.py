b39390fd75bb7ba35acc9ee25113a31f25345790
Luis M. Santos <luis.santos2@nih.gov>
2025-05-02 15:50:09 -0400

    Updated unit test expected values for the ACR Slice Thickness. Added new unit tests to help prevent my recent mistake of using the same width value for both top and bottom ramp, which lead to credible results that are wrong.

--- tests/test_acr_slice_thickness.py
+++ tests/test_acr_slice_thickness.py
@@ -11,8 +11,11 @@ from tests import TEST_DATA_DIR
 
 class TestACRSliceThicknessSiemens(unittest.TestCase):
     ACR_DATA = pathlib.Path(TEST_DATA_DIR / "acr" / "Siemens")
-    centers = [(44.0, 2.0), (46.0, 2.0)]
-    dz = 5.18
+    centers = [(75.0, 2.0), (77.0, 2.0)]
+    dz = 5.44
+    dz_3x = 7.97
+    top_dz = 5.2
+    bottom_dz = 5.7
 
     def setUp(self):
         input_files = get_dicom_files(self.ACR_DATA)
@@ -24,6 +27,18 @@ class TestACRSliceThicknessSiemens(unittest.TestCase):
             self.dcm.pixel_array, self.dcm.PixelSpacing[0], self.dcm.PixelSpacing[1]
         )
         self.results = self.acr_slice_thickness_task.get_slice_thickness(self.dcm)
+        self.large_thickness = self.acr_slice_thickness_task.compute_thickness(
+            self.results['ramps']['top']['width'],
+            self.results['ramps']['bottom']['width'] * 3
+        )
+        self.top_thickness = self.acr_slice_thickness_task.compute_thickness(
+            self.results['ramps']['top']['width'],
+            self.results['ramps']['top']['width']
+        )
+        self.bottom_thickness = self.acr_slice_thickness_task.compute_thickness(
+            self.results['ramps']['bottom']['width'],
+            self.results['ramps']['bottom']['width']
+        )
 
     def test_slice_thickness(self):
         slice_thickness_val = round(
@@ -45,14 +60,64 @@ class TestACRSliceThicknessSiemens(unittest.TestCase):
 
         assert centers == self.centers
 
+    def test_exagerated_slice_thickness(self):
+        """ This is test is meant to ensure that the formula was implemented properly. """
+        slice_thickness_val = round(
+            self.large_thickness, 2
+        )
+
+        print("\ntest_slice_thickness.py::TestSliceThickness::test_exagerated_slice_thickness")
+        print("new_release_value:", slice_thickness_val)
+        print("fixed_value:", self.dz_3x)
+
+        assert slice_thickness_val == self.dz_3x
+
+    def test_top_ramp_only(self):
+        """ This is test is meant to ensure that the formula was implemented properly. """
+        slice_thickness_val = round(
+            self.results['thickness'], 2
+        )
+        top_slice_thickness_val = round(
+            self.top_thickness, 2
+        )
+
+        print("\ntest_slice_thickness.py::TestSliceThickness::test_top_ramp_only")
+        print("new_release_value:", slice_thickness_val)
+        print("new_release_value (top only):", top_slice_thickness_val)
+        print("fixed_value:", self.top_dz)
+
+        assert not (slice_thickness_val == top_slice_thickness_val and slice_thickness_val == self.top_dz)
+
+    def test_bottom_ramp_only(self):
+        """ This is test is meant to ensure that the formula was implemented properly. """
+        slice_thickness_val = round(
+            self.results['thickness'], 2
+        )
+        bottom_slice_thickness_val = round(
+            self.bottom_thickness, 2
+        )
+
+        print("\ntest_slice_thickness.py::TestSliceThickness::test_top_ramp_only")
+        print("new_release_value:", slice_thickness_val)
+        print("new_release_value (bottom only):", bottom_slice_thickness_val)
+        print("fixed_value:", self.bottom_dz)
+
+        assert not (slice_thickness_val == bottom_slice_thickness_val and slice_thickness_val == self.bottom_dz)
+
 
 class TestACRSliceThicknessPhilipsAchieva(TestACRSliceThicknessSiemens):
     ACR_DATA = pathlib.Path(TEST_DATA_DIR / "acr" / "PhilipsAchieva")
-    centers = [(46.0, 2.0), (39.0, 2.0)]
-    dz = 4.69
+    centers = [(79.0, 2.0), (72.0, 2.0)]
+    dz = 5.59
+    dz_3x = 7.75
+    top_dz = 4.8
+    bottom_dz = 6.7
 
 
 class TestACRSliceThicknessSiemensSolaFit(TestACRSliceThicknessSiemens):
     ACR_DATA = pathlib.Path(TEST_DATA_DIR / "acr" / "SiemensSolaFit")
-    centers = [(44.0, 2.0), (48.0, 2.0)]
-    dz = 5.18
+    centers = [(75.0, 2.0), (80.0, 2.0)]
+    dz = 5.03
+    dz_3x = 7.29
+    top_dz = 4.7
+    bottom_dz = 5.4
